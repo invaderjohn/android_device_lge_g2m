7d0d9da3a15fe1b6104eac08e465b318ba16861d
john <invaderjohn@users.noreply.github.com>
2015-02-24 11:42:40 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8226
 # Kernel image
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_CUSTOM_BOOTIMG_MK := device/lge/g2m/mkbootimg.mk
-TARGET_KERNEL_SOURCE := kernel/lge/lge_msm8226
+TARGET_KERNEL_SOURCE := kernel/lge/msm8226
 TARGET_KERNEL_CONFIG := Kool-aid_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=31 msm_rtb.filter=0x37 androidboot.hardware=g2m
 BOARD_KERNEL_BASE := 0x00000000
